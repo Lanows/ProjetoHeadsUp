b8e3205f1c70d41581257d40e1c56032ccf89234
Herculano Washington Silva <hercubaws@gmail.com>
2019-05-17 21:28:56 -0300

    Telas Feitas, faltando video e foto

--- app/src/main/java/com/example/projetoheadsup/TelaInfo.java
+++ app/src/main/java/com/example/projetoheadsup/TelaInfo.java
@@ -1,7 +1,11 @@
 package com.example.projetoheadsup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class TelaInfo extends AppCompatActivity {
 
@@ -9,5 +13,33 @@ public class TelaInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_info);
+
+        ImageButton btnYoutube = (ImageButton) findViewById(R.id.btnYoutube);
+        ImageButton btnWordpress = (ImageButton) findViewById(R.id.btnWordpress);
+        ImageButton btnInstagram = (ImageButton) findViewById(R.id.btnInstagram);
+
+        btnYoutube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent next = new Intent(TelaInfo.this, TelaModo.class);
+                startActivity(next);
+            }
+        });
+
+        btnWordpress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent next = new Intent(TelaInfo.this, TelaInfo2.class);
+                startActivity(next);
+            }
+        });
+
+        btnInstagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent next = new Intent(TelaInfo.this, TelaModo.class);
+                startActivity(next);
+            }
+        });
     }
 }
